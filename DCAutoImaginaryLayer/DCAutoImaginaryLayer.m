9107025eead147c8f15913d2f05c1c1c90a44be3
XDChang— <1154236089@qq.com>
2018-07-10 10:03:34 +0800

    xdc：代码优化，限制最低，最大滑动值。

--- DCAutoImaginaryLayer/DCAutoImaginaryLayer.m
+++ DCAutoImaginaryLayer/DCAutoImaginaryLayer.m
@@ -15,23 +15,27 @@
     CGPoint _endPoint;
     UIBezierPath *_path;
     CAShapeLayer *_shapeLayer;
+    UIView *_redView;
+    BOOL _canMove;
+    CGFloat _lastX;
+    UIImageView *_img;
 }
 
 - (instancetype)initWithFrame:(CGRect)frame{
     self = [super initWithFrame:frame];
     if (self) {
-        
+        _canMove = NO;
         self.backgroundColor = [UIColor yellowColor];
         _shapeLayer = [CAShapeLayer layer];
         _shapeLayer.frame = self.bounds;
         _shapeLayer.path = _path.CGPath;
         _shapeLayer.fillColor = [UIColor clearColor].CGColor;
+        
+        _shapeLayer.strokeColor = [UIColor colorWithRed:119/255.0 green:213/255.0 blue:114/255.0 alpha:1].CGColor;
+        //每个虚线宽度为2，间隔为3 整个线条宽度为30
         _shapeLayer.lineWidth = 30.0f;
-        _shapeLayer.strokeColor = [UIColor colorWithRed:141/255.0 green:175/255.0 blue:40/255.0 alpha:1].CGColor;
-        //每个虚线宽度为2，间隔为3
         _shapeLayer.lineDashPattern = [NSArray arrayWithObjects:[NSNumber numberWithInt:4], [NSNumber numberWithInt:3], nil];
         [self.layer addSublayer:_shapeLayer];
-        
         UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(frame.size.width/2 - 90, frame.size.height/2 - 90, 180, 180)];
         imageView.image = [UIImage imageNamed:@"volume_text_area"];//volume_text_area
         imageView.layer.cornerRadius = 90;
@@ -43,6 +47,23 @@
         UIImage *maskImage = [UIImage imageNamed:@"volume_area"];
         maskLayer.contents = (__bridge id)maskImage.CGImage;
         self.layer.mask = maskLayer;
+        
+        UIView *redView = [[UIView alloc]initWithFrame:CGRectMake(frame.size.width/2 - 80, frame.size.height/2 - 80, 160, 160)];
+        redView.backgroundColor = [UIColor clearColor];
+        [self addSubview:redView];
+        _redView = redView;
+        
+        UIImageView *img = [[UIImageView alloc]initWithFrame:CGRectMake(15, 68, 24, 24)];
+        img.image = [UIImage imageNamed:@"button"];
+        [_redView addSubview:img];
+        _img = img;
+        
+        _redView.layer.anchorPoint = CGPointMake(0.5, 0.5);
+        CATransform3D rotatedTransform = _redView.layer.transform;
+        rotatedTransform = CATransform3DMakeRotation(2.15-M_PI, 0, 0, 1.0);//rotate为旋转弧度，绕Z轴转
+        _redView.layer.transform = rotatedTransform;
+        
+        
     }
     return self;
 }
@@ -78,6 +99,17 @@
     //    }
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    UITouch *touch = [touches anyObject];
+    CGPoint touchPoint = [touch locationInView:self];
+    touchPoint = [_img.layer convertPoint:touchPoint fromLayer:self.layer];
+    NSLog(@"touchPoint==%.f",touchPoint.x);
+    if ([_img.layer containsPoint:touchPoint]) {
+        _canMove = YES;
+    }
+}
+
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     UITouch *touch = [touches anyObject];
     CGPoint touchPoint = [touch locationInView:self];
@@ -95,13 +127,69 @@
         angle = M_PI + a;
     }
     if (x > 0 && y > 0) {
-        angle =  a;
+        angle = a;
     }
     if (x < 0 && y > 0) {
         angle = a;
     }
     // ========== 4个象限角度的处理 end =============//
-    _path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(self.frame.size.width/2, self.frame.size.height/2) radius:90 startAngle:-M_PI endAngle:angle clockwise:1];
-    _shapeLayer.path = _path.CGPath;
+    
+    
+    NSLog(@"sss%.2f",M_PI - angle);
+    
+    // ================= 以下是新增的 view 旋转 =================//
+    /*
+     范围在 -3.14 ~~~~~~~ 3.14 (M_PI - angle 得到 2π~0 区间范围)
+     在 0.72 ~~~~ 2 不旋转 范围自定义
+     
+     
+     
+     */
+    
+    
+    if (0.72 < angle && angle < 2) {
+        _canMove = NO;
+    }
+    
+    if (_canMove) {
+        
+        _path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(self.frame.size.width/2, self.frame.size.height/2) radius:90 startAngle:2 endAngle:angle clockwise:1];
+        _shapeLayer.path = _path.CGPath;
+        
+        _redView.layer.anchorPoint = CGPointMake(0.5, 0.5);
+        CATransform3D rotatedTransform = _redView.layer.transform;
+        rotatedTransform = CATransform3DMakeRotation(angle-M_PI, 0, 0, 1.0);//rotate为旋转弧度，绕Z轴转
+        _redView.layer.transform = rotatedTransform;
+    }
+    
+    
+    
+    // ================= view 旋转 end ======================== //
+    
+}
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    
+    CGAffineTransform _trans = _redView.transform;
+    CGFloat rotate = acosf(_trans.a);
+    NSLog(@"%.2f",rotate);
+    NSString *str = [NSString stringWithFormat:@"%.f",rotate];
+    if ([str isEqualToString:@"2"]) {
+        _redView.layer.anchorPoint = CGPointMake(0.5, 0.5);
+        CATransform3D rotatedTransform = _redView.layer.transform;
+        rotatedTransform = CATransform3DMakeRotation(0.70-M_PI, 0, 0, 1.0);//rotate为旋转弧度，绕Z轴转
+        _redView.layer.transform = rotatedTransform;
+        
+    }
+    if ([str isEqualToString:@"1"]) {
+        _redView.layer.anchorPoint = CGPointMake(0.5, 0.5);
+        CATransform3D rotatedTransform = _redView.layer.transform;
+        rotatedTransform = CATransform3DMakeRotation(2.15-M_PI, 0, 0, 1.0);//rotate为旋转弧度，绕Z轴转
+        _redView.layer.transform = rotatedTransform;
+        
+    }
+    
+    
 }
 @end
+
